82257d70e4184e9908cd0edc214d86403f8e8c8c
dArk10R4 <106441625+dArk10R4@users.noreply.github.com>
2023-12-01 18:14:32 +0400

    Main h

---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+/**
+ * struct print_special - prints
+ * @printer - function to print
+ */
+
+typedef struct print_special
+{
+    int (*printer)(va_list *);
+} prnt;
+int print_int(int arg);
+void handle_int(va_list *arg);
+void default_handler(va_list *a);
+int print_char(char c);
+void handle_char(va_list *args);
+int print_string(char *str);
+void handle_str(va_list *args);
+int handle_type(char c, prnt *printer);
+int _printf(char *format, ...);
+
+#endif
